0f058c09f9c028abccc5a3e0548294bd44dc972e
KypT <0KypT0@gmail.com>
2015-03-07 05:02:44 +0500

    added gallery

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,7 +11,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( bootstrap-theme.min.css )
-Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 Rails.application.config.assets.precompile += %w( fileinput.css )
 Rails.application.config.assets.precompile += %w( fileinput.js )
