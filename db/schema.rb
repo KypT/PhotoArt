7fb0ac4c050c250ad4ec97dff60c94d5b496e409
Kocherov Kirill <kirill@xtalk.msk.su>
2015-06-03 17:35:27 +0300

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603012739) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20150603142744) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
