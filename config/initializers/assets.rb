f49c1081accc786de13710f443289edf860a1743
KypT <0KypT0@gmail.com>
2015-06-18 21:39:28 +0300

    site update

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w(*.js *.scss)
-Rails.application.config.assets.precompile += %w(*.jpg *.png *.gif)
+%w( main gallery events articles admin ).each do |controller|
+  Rails.application.config.assets.precompile += ["#{controller}.js.coffee", "#{controller}.css"]
+end
