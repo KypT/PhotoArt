871bf69f38a30e199571dec951697c838d9b5707
KypT <0KypT0@gmail.com>
2015-06-19 15:39:31 +0300

    adaptive update

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,20 @@
-$(document).on('page:change', function()
-{
+$( function() {
+    var sections = $('.section');
+
+    var scaleSections = function () {
+        if ($(window).width() < 440)
+            sections.removeClass('col-xs-6').addClass('col-sm-6');
+        else if ($(window).width() < 930)
+            sections.removeClass('col-sm-4 col-xs-6').addClass('col-xs-6');
+        else
+            sections.removeClass('col-xs-6').addClass('col-sm-4');
+    };
+
+    var resizeTimer;
+    $(window).resize(function() {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(scaleSections, 100);
+    });
+
+    scaleSections();
 });
