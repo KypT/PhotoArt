fa15c9695dddd13aa11c68077a2a42a37103e8fd
KypT <0KypT0@gmail.com>
2015-06-19 16:35:00 +0300

    more adaptive work

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,11 +3,11 @@ $( function() {
 
     var scaleSections = function () {
         if ($(window).width() < 440)
-            sections.removeClass('col-xs-6').addClass('col-sm-6');
+            sections.removeClass('col-xs-6 col-sm-4').addClass('col-sm-6');
         else if ($(window).width() < 930)
             sections.removeClass('col-sm-4 col-xs-6').addClass('col-xs-6');
         else
-            sections.removeClass('col-xs-6').addClass('col-sm-4');
+            sections.removeClass('col-xs-6 col-sm-6').addClass('col-sm-4');
     };
 
     var resizeTimer;
