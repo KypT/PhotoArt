80b05a33d45d7516fd97d539ef1e03ae16fa6d63
KypT <0KypT0@gmail.com>
2015-06-20 13:49:08 +0300

    added sitemap.xml

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -10,4 +10,11 @@ class MainController < ApplicationController
     PhotoMailer.send_message_to_author(params).deliver
     respond_to {|format| format.js }
   end
+
+  def sitemap
+    @sections = Section.all
+    @articles = Article.all
+    @events = Event.all
+    @albums = Album.all
+  end
 end
