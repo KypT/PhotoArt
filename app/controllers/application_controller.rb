21209208d60cc8f73ef93f288c91d39ed3d3146e
= <0KypT0@gmail.com>
2015-08-03 14:55:18 +0500

    added 404 and 500 error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :load_sections
+  rescue_from Exception, :with => :error_500
+  rescue_from ActionController::RoutingError, :with => :error_404
 
   def load_sections
     @sections = Section.all
@@ -12,6 +14,15 @@ class ApplicationController < ActionController::Base
   def authenticate_admin!
     authenticate_user!
     not_found unless current_user.admin?
+  end
+
+  def error_404
+    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+  end
+
+
+  def error_500
+    render file: "#{Rails.root}/public/500.html", layout: false, status: 500
   end
 
   def not_found
